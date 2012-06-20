b50fc74d243c54462de63463371227aee1e2e75c
Masaru Hashizume <hashizume.masaru@gmail.com>
2012-06-20 23:23:48 +0900

    Andorid create FlashLight Sample

---
+++ src/me/gensan/android/samplelight/SampleLightActivity.java
@@ -0,0 +1,57 @@
+package me.gensan.android.samplelight;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class SampleLightActivity extends Activity implements Callback {
+	
+	private Camera mCamera;
+	private SurfaceView mSurfaceview;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // SurfaceViewを設定
+        mSurfaceview = new SurfaceView(getApplicationContext());
+        SurfaceHolder holder = mSurfaceview.getHolder();
+        holder.addCallback(this);
+        setContentView(mSurfaceview);
+    }
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		mCamera = Camera.open();
+		Parameters parameters = mCamera.getParameters();
+		parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		mCamera.setParameters(parameters);
+		try {
+			mCamera.setPreviewDisplay(mSurfaceview.getHolder());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mCamera.startPreview();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {}
+
+	@Override
+	protected void onStop() {
+		if (mCamera != null) {
+			mCamera.stopPreview();
+			mCamera.release();
+			mCamera = null;
+		}
+		super.onDestroy();
+	}
+}
